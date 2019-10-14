76469ffb24b4b4865546d43f4833f18062c8ed45
Maxime Wierez-Kien <maxime.wierez-kien@epitech.eu>
2019-10-14 15:04:21 +0200

    Expanded resources strings

--- ServerConnect/app/src/main/java/com/example/cashmanager/ui/bill/BillActivity.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/ui/bill/BillActivity.kt
@@ -1,13 +1,21 @@
 package com.example.cashmanager.ui.bill
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.cashmanager.R
+import com.example.cashmanager.ui.payment.PaymentActivity
 
 class BillActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bill)
+    }
+
+    fun goToPayment(v: View) {
+        val intent = Intent(this, PaymentActivity::class.java)
+        startActivity(intent)
     }
 }
--- ServerConnect/app/src/main/java/com/example/cashmanager/ui/cashRegister/CashRegisterActivity.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/ui/cashRegister/CashRegisterActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.cashmanager.R
+import com.example.cashmanager.ui.bill.BillActivity
 import com.example.cashmanager.ui.productPicker.ProductPickerActivity
 
 class CashRegisterActivity : AppCompatActivity() {
@@ -16,6 +17,21 @@ class CashRegisterActivity : AppCompatActivity() {
 
     fun openProductPicker(v: View) {
         val intent = Intent(this, ProductPickerActivity::class.java)
+        startActivity(intent)
+    }
+
+    /**
+     * Reset the content of the cart
+     */
+    fun reset(v: View) {
+        // Todo: clear the list of products in the cart
+    }
+
+    /**
+     * Proceed to the next activity, displaying the bill total
+     */
+    fun proceed(v: View) {
+        val intent = Intent(this, BillActivity::class.java)
         startActivity(intent)
     }
 }
