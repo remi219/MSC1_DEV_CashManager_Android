56a1b360305a4feffa75cab89e08e40378c57c2c
Maxime Wierez-Kien <maxime.wierez-kien@epitech.eu>
2019-10-14 10:26:49 +0200

    Added base for adapter, added row layout

--- ServerConnect/app/src/main/java/com/example/cashmanager/ui/cashRegister/CashRegisterAdapter.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/ui/cashRegister/CashRegisterAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.cashmanager.ui.cashRegister
 
-class CashRegisterAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CashRegisterAdapter : RecyclerView.Adapter<CashRegisterAdapter.ProductViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ProductViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+    }
 }
--- ServerConnect/app/src/main/java/com/example/cashmanager/ui/payment/PaymentActivity.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/ui/payment/PaymentActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.cashmanager.R
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 
 class PaymentActivity : AppCompatActivity() {
 
@@ -14,8 +16,27 @@ class PaymentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_payment)
     }
 
-    fun scanQRcode(v: View) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
+        val result: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if(result != null){
+
+            if(result.contents != null){
+                // Todo: => payment success !
+                // txtValue.text = result.contents
+            } else {
+                // txtValue.text = "scan failed"
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    fun scanQRcode(v: View) {
+        run {
+            IntentIntegrator(this@PaymentActivity).initiateScan()
+        }
     }
 
     fun scanNFC(v: View) {
