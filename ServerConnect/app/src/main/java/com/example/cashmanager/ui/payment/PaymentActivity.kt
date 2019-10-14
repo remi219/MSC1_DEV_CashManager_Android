7293fc334f13ca10352a8a37efe6c8ac680cbf67
Maxime Wierez-Kien <maxime.wierez-kien@epitech.eu>
2019-10-14 11:04:26 +0200

    Migrated to AndroidX. Added NFC and QRcode scan

--- ServerConnect/app/src/main/java/com/example/cashmanager/ui/payment/PaymentActivity.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/ui/payment/PaymentActivity.kt
@@ -2,14 +2,23 @@ package com.example.cashmanager.ui.payment
 
 import android.app.Activity
 import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import com.example.cashmanager.R
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
+import java.nio.charset.Charset
+import android.widget.Toast
 
-class PaymentActivity : AppCompatActivity() {
+class PaymentActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+
+    var NFCscanActive : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +42,33 @@ class PaymentActivity : AppCompatActivity() {
         }
     }
 
+    override fun onTagDiscovered(tag: Tag?) {
+        if (!NFCscanActive)
+            return
+
+        println("Detected tag !")
+
+        // convert tag to NDEF tag
+        val ndef = Ndef.get(tag)
+        ndef?.connect()
+        val ndefMessage = ndef?.ndefMessage ?: ndef?.cachedNdefMessage
+        val message = ndefMessage?.toByteArray()
+            ?.toString(Charset.forName("UTF-8")) ?: ""
+        //val id = tag?.id?.toString(Charset.forName("ISO-8859-1")) ?: ""
+        //val id = bytesToHexString(tag?.id) ?: ""
+        ndef?.close()
+        //val data = mapOf(kId to id, kContent to message, kError to "", kStatus to "reading")
+        val mainHandler = Handler(Looper.getMainLooper())
+
+        this.runOnUiThread {
+            Toast.makeText(this, "NFC detected", Toast.LENGTH_SHORT).show()
+        }
+        if (message != "")
+            println(message)
+        else
+            println("No message")
+    }
+
     fun scanQRcode(v: View) {
         run {
             IntentIntegrator(this@PaymentActivity).initiateScan()
@@ -40,7 +76,7 @@ class PaymentActivity : AppCompatActivity() {
     }
 
     fun scanNFC(v: View) {
-
+        NFCscanActive = true
     }
 
     fun backToRegister(v : View) {
