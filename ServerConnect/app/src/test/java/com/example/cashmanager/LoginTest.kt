7a774151399a09b816e27338cbf807ed716f9de1
remi correnson <remi.correnson@epitech.eu>
2019-10-15 09:33:47 +0200

    tests first draft

--- ServerConnect/app/src/test/java/com/example/cashmanager/LoginTest.kt
+++ ServerConnect/app/src/test/java/com/example/cashmanager/LoginTest.kt
@@ -1,2 +1,20 @@
 package com.example.cashmanager
 
+import com.example.cashmanager.data.LoginDataSource
+import org.junit.Test
+import org.junit.Assert.*
+
+class LoginTest {
+    @Test
+    fun testLoginSuccess() {
+        val ipTest = "120.120.10.10"
+        val pwdTest = "pwdTest"
+        val loginStatus = LoginDataSource.login(ipTest, pwdTest)
+        assertEquals("SUCCESS", loginStatus)
+    }
+
+    @Test
+    fun testLoginFail() {
+
+    }
+}
