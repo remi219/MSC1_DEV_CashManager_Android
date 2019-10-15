2285a0e9718e734c4c0515eb5d480d16ad835545
remi correnson <remi.correnson@epitech.eu>
2019-10-15 11:32:45 +0200

    add tests dependencies

--- ServerConnect/app/src/test/java/com/example/cashmanager/LoginTest.kt
+++ ServerConnect/app/src/test/java/com/example/cashmanager/LoginTest.kt
@@ -1,6 +1,8 @@
 package com.example.cashmanager
 
 import com.example.cashmanager.data.LoginDataSource
+import com.example.cashmanager.data.Result
+import com.example.cashmanager.data.model.ServerData
 import org.junit.Test
 import org.junit.Assert.*
 
@@ -9,8 +11,8 @@ class LoginTest {
     fun testLoginSuccess() {
         val ipTest = "120.120.10.10"
         val pwdTest = "pwdTest"
-        val loginStatus = LoginDataSource.login(ipTest, pwdTest)
-        assertEquals("SUCCESS", loginStatus)
+        val res : Result<ServerData> = LoginDataSource.login(ipTest, pwdTest)
+        assertEquals("SUCCESS", res)
     }
 
     @Test
