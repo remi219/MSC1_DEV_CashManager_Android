d893caf2c8541397c6c4df52433213a11100e568
remi correnson <remi.correnson@epitech.eu>
2019-10-21 09:52:08 +0200

    tests for login and cart

--- ServerConnect/app/src/test/java/com/example/cashmanager/LoginTest.kt
+++ ServerConnect/app/src/test/java/com/example/cashmanager/LoginTest.kt
@@ -7,16 +7,30 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class LoginTest {
+
+    private val lds : LoginDataSource = LoginDataSource()
+
     @Test
     fun testLoginSuccess() {
         val ipTest = "120.120.10.10"
         val pwdTest = "pwdTest"
-        val res : Result<ServerData> = LoginDataSource.login(ipTest, pwdTest)
+        val res : Result<ServerData> = lds.login(ipTest, pwdTest)
         assertEquals("SUCCESS", res)
     }
 
     @Test
-    fun testLoginFail() {
+    fun testLoginFailWrongIp() {
+        val ipTest = "wrongip"
+        val pwdTest = "pwdTest"
+        val res : Result<ServerData> = lds.login(ipTest, pwdTest)
+        assertEquals("Error logging in", res)
+    }
 
+    @Test
+    fun testLoginFailToShortPwd() {
+        val ipTest = "120.120.10.10"
+        val pwdTest = "123"
+        val res : Result<ServerData> = lds.login(ipTest, pwdTest)
+        assertEquals("ERROR", res)
     }
 }
