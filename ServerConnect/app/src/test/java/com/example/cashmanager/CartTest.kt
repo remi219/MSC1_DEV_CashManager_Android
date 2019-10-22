a52844d0278f4b3bbbffd59dd2ae29d4aefb3a6d
remi correnson <remi.correnson@epitech.eu>
2019-10-22 10:41:30 +0200

    fix test Cart reset

--- ServerConnect/app/src/test/java/com/example/cashmanager/CartTest.kt
+++ ServerConnect/app/src/test/java/com/example/cashmanager/CartTest.kt
@@ -4,15 +4,15 @@ import com.example.cashmanager.data.model.Product
 import com.example.cashmanager.data.model.Cart
 import org.junit.Test
 import org.junit.Assert.*
+import java.lang.Boolean.TRUE
 
 class CartTest {
     private val cart : Cart = Cart()
 
     @Test
     fun testReset() {
-        val products: MutableList<Pair<Product, Int>> = mutableListOf()
-        products.add(Pair(Product(1, "TestProduct", "34.50".toFloat()), 1))
+        cart.products.add(Pair(Product(1, "TestProduct", "34.50".toFloat()), 1))
         cart.reset()
-        assert(products.isEmpty())
+        assertEquals(TRUE, cart.products.isEmpty())
     }
 }
