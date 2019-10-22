89fe4747b0a34fa47c38083d08f3e6371375841a
Maxime Wierez-Kien <maxime.wierez-kien@epitech.eu>
2019-10-22 10:18:24 +0200

    Working cancel an article button

--- ServerConnect/app/src/main/java/com/example/cashmanager/di/AppModule.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/di/AppModule.kt
@@ -3,21 +3,24 @@ package com.example.cashmanager.di
 import com.example.cashmanager.service.OrderService
 import com.example.cashmanager.service.PaymentService
 import com.example.cashmanager.service.ProductService
+import com.example.cashmanager.service.StatusService
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private val baseURl = "http://localhost:4242"
+private const val baseURl = "http://localhost:4242"
 private val retrofit: Retrofit = Retrofit.Builder()
     .baseUrl(baseURl)
     .addConverterFactory(GsonConverterFactory.create())
     .build()
 
+private val statusAPI = retrofit.create(StatusService::class.java)
 private val productAPI = retrofit.create(ProductService::class.java)
 private val paymentAPI = retrofit.create(PaymentService::class.java)
 private val orderAPI = retrofit.create(OrderService::class.java)
 
 val appModule = module {
+    single {statusAPI}
     single {productAPI}
     single {paymentAPI}
     single {orderAPI}
--- ServerConnect/app/src/main/java/com/example/cashmanager/ui/bill/BillActivity.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/ui/bill/BillActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -28,11 +29,13 @@ class BillActivity : AppCompatActivity() {
 
     lateinit var cart : Cart
     lateinit var paymentMode : PaymentMode
+    private val activity = this
     private val orderAPI : OrderService by inject()
 
     lateinit var cartRecyclerView : RecyclerView
     lateinit var totalTextView : TextView
     lateinit var paymentLabel : TextView
+    lateinit var progressView : FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +44,7 @@ class BillActivity : AppCompatActivity() {
         cartRecyclerView = findViewById(R.id.product_list)
         totalTextView = findViewById(R.id.bill_total)
         paymentLabel = findViewById(R.id.payment_mode)
+        progressView = findViewById(R.id.progress_view)
 
         cart = intent.getSerializableExtra("cart") as Cart? ?: Cart()
         paymentMode = intent.getSerializableExtra("paymentMode") as PaymentMode
@@ -61,16 +65,21 @@ class BillActivity : AppCompatActivity() {
 
     fun goToPayment(v: View) {
         val intent = Intent(this, PaymentActivity::class.java)
+        intent.putExtra("cart", cart)
         intent.putExtra("paymentMode", paymentMode)
+
+        progressView.visibility = View.VISIBLE
 
         orderAPI.createUserOrder(1, OrderDTO()).enqueue(object : Callback<OrderDTO> {
             override fun onResponse(call: Call<OrderDTO>, response: Response<OrderDTO>) {
                 startActivity(intent)
+                progressView.visibility = View.GONE
             }
 
             override fun onFailure(call: Call<OrderDTO>, t: Throwable) {
                 startActivity(intent)
-                //Toast.makeText(this, "Could not make order", Toast.LENGTH_SHORT).show()
+                progressView.visibility = View.GONE
+                Toast.makeText(activity, "Could not make order", Toast.LENGTH_SHORT).show()
             }
         })
     }
