0fd557c686bad03dd4220e097f4eabbb32e32b76
remi correnson <remi.correnson@epitech.eu>
2019-11-01 16:19:21 +0100

    activity login

--- ServerConnect/app/src/main/java/com/example/cashmanager/ui/login/LoginViewModel.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/ui/login/LoginViewModel.kt
@@ -29,7 +29,7 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
 
     fun loginDataChanged(username: String, password: String) {
         if (!isUsernameValid(username)) {
-            _loginForm.value = LoginFormState(usernameError = R.string.invalid_ip)
+            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
         } else if (!isPasswordValid(password)) {
             _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
         } else {
