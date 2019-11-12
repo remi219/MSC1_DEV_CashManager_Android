647b56d0ff1cb3121ae87ed472279d5e00035023
Maxime Wierez-Kien <maxime.wierez-kien@epitech.eu>
2019-11-12 16:04:58 +0100

    Attempt fixing NFC with foreground dispatch

--- ServerConnect/app/src/main/java/com/example/cashmanager/ui/payment/PaymentActivity.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/ui/payment/PaymentActivity.kt
@@ -1,12 +1,18 @@
 package com.example.cashmanager.ui.payment
 
+import android.app.PendingIntent
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.content.IntentFilter
 import android.net.Uri
 import android.nfc.NfcAdapter
+import android.nfc.NfcAdapter.ACTION_TECH_DISCOVERED
 import android.nfc.Tag
+import android.nfc.tech.IsoDep
 import android.nfc.tech.Ndef
+import android.nfc.tech.NdefFormatable
+import android.nfc.tech.NfcA
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -18,6 +24,7 @@ import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import java.nio.charset.Charset
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.example.cashmanager.data.dto.PaymentCardDTO
 import com.example.cashmanager.data.dto.PaymentChequeDTO
@@ -36,10 +43,14 @@ import java.lang.Exception
 import java.text.NumberFormat
 import kotlin.math.round
 
-class PaymentActivity : AppCompatActivity(), NfcAdapter.ReaderCallback,
+class PaymentActivity : AppCompatActivity(),
     ConnectionStatusFragment.OnFragmentInteractionListener {
 
-    private var NFCscanActive : Boolean = false
+    private lateinit var pendingIntent : PendingIntent
+    private lateinit var intentFiltersArray : Array<IntentFilter>
+    private lateinit var techListsArray : Array<Array<String>>
+
+    private lateinit var nfcAdapter: NfcAdapter
     private lateinit var billTextView: TextView
     private lateinit var statusTextView : TextView
     private lateinit var scanChequeBtn : Button
@@ -55,6 +66,8 @@ class PaymentActivity : AppCompatActivity(), NfcAdapter.ReaderCallback,
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
 
         billTextView = findViewById(R.id.bill_total)
         statusTextView = findViewById(R.id.payment_status_label)
@@ -81,6 +94,31 @@ class PaymentActivity : AppCompatActivity(), NfcAdapter.ReaderCallback,
             scanNFCBtn.visibility = View.VISIBLE
             nfcCheck()
         }
+
+        // See https://stackoverflow.com/questions/21307898/how-to-proactive-read-nfc-tag-without-intent
+        // https://developer.android.com/guide/topics/connectivity/nfc/advanced-nfc
+        val intent = Intent(this, this.javaClass)
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
+        try {
+            // Handles all MIME based dispatches. You should specify only the ones that you need.
+            ndef.addDataType("*/*")
+        } catch (e: IntentFilter.MalformedMimeTypeException) {
+            throw RuntimeException("failed to add MIME type", e);
+        }
+        // Use no intent filters to accept all MIME types
+        intentFiltersArray = Array(1) {ndef}
+
+        // The tech list array can be set to null to accept all types of tag
+        techListsArray = arrayOf(
+            arrayOf(
+                IsoDep::class.java.name,
+                NfcA::class.java.name,
+                NdefFormatable::class.java.name
+            )
+        )
     }
 
     /**
@@ -102,47 +140,53 @@ class PaymentActivity : AppCompatActivity(), NfcAdapter.ReaderCallback,
         }
     }
 
-    override fun onTagDiscovered(tag: Tag?) {
-        if (!NFCscanActive)
-            return
+    override fun onPause() {
+        println("On Pause")
+        nfcAdapter.disableForegroundDispatch(this)
+        super.onPause()
+    }
 
-        println("Detected tag !")
+    override fun onResume() {
+        println("On Resume")
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray)
+        super.onResume()
+    }
 
-        // convert tag to NDEF tag
-        val ndef = Ndef.get(tag)
-        ndef?.connect()
-        val ndefMessage = ndef?.ndefMessage ?: ndef?.cachedNdefMessage
-        val message = ndefMessage?.toByteArray()
-            ?.toString(Charset.forName("UTF-8")) ?: ""
-        //val id = tag?.id?.toString(Charset.forName("ISO-8859-1")) ?: ""
-        //val id = bytesToHexString(tag?.id) ?: ""
-        ndef?.close()
-        //val data = mapOf(kId to id, kContent to message, kError to "", kStatus to "reading")
-        val mainHandler = Handler(Looper.getMainLooper())
-
-        this.runOnUiThread {
-            Toast.makeText(this, "NFC detected $message", Toast.LENGTH_SHORT).show()
-        }
-        if (message != "")
-            println(message)
-        else
-            println("No message")
-        sendCardPayment(message)
+    override fun onNewIntent(intent: Intent) {
+        println("Foreground dispatch")
+        val nfcTag : Tag?  = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        println(nfcTag.toString())
+        super.onNewIntent(intent)
     }
 
     /**
      * Check if nfc is available and enabled
      */
-    fun nfcCheck() {
+    private fun nfcCheck() {
         val nfcAndroidAdapter = NfcAdapter.getDefaultAdapter(this)
 
         if (nfcAndroidAdapter == null) {
-            Toast.makeText(this, resources.getString(R.string.nfc_not_supported), Toast.LENGTH_LONG).show()
-        } else {
-            if (!nfcAndroidAdapter.isEnabled) {
-                Toast.makeText(this, resources.getString(R.string.nfc_not_enable), Toast.LENGTH_LONG).show()
+            val builder = AlertDialog.Builder(this)
+            with(builder)
+            {
+                setIcon(android.R.drawable.ic_dialog_alert)
+                setTitle(R.string.nfc_payment)
+                setMessage(R.string.nfc_not_supported)
+                setPositiveButton(R.string.ok){ _, _ -> finish() }
+                show()
             }
-        }
+        } else if (!nfcAndroidAdapter.isEnabled) {
+            val builder = AlertDialog.Builder(this)
+            with(builder)
+            {
+                setIcon(android.R.drawable.ic_dialog_alert)
+                setTitle(R.string.nfc_payment)
+                setMessage(R.string.nfc_not_enable)
+                setPositiveButton(R.string.ok){ _, _ ->}
+                show()
+            }
+        } else
+            return
     }
 
 
@@ -159,7 +203,7 @@ class PaymentActivity : AppCompatActivity(), NfcAdapter.ReaderCallback,
      * Start NFC scanning
      */
     fun scanNFC(v: View) {
-        NFCscanActive = true
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray)
     }
 
     /**
