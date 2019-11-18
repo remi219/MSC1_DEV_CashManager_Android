0160ee32bcb01457a939af80937f891523db49e8
Maxime Wierez-Kien <maxime.wierez-kien@epitech.eu>
2019-11-18 17:00:07 +0100

    Started register methods

--- ServerConnect/app/src/main/java/com/example/cashmanager/service/LoginService.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/service/LoginService.kt
@@ -1,6 +1,8 @@
 package com.example.cashmanager.service
 
 import com.example.cashmanager.data.dto.LoginDTO
+import com.example.cashmanager.data.dto.UserDTO
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -12,4 +14,7 @@ interface LoginService {
      */
     @POST("login")
     fun login(@Body login: LoginDTO) : Call<String>
+
+    @POST("register")
+    fun register(@Body user: UserDTO) : Call<ResponseBody>
 }
--- ServerConnect/app/src/main/java/com/example/cashmanager/ui/register/RegisterActivity.kt
+++ ServerConnect/app/src/main/java/com/example/cashmanager/ui/register/RegisterActivity.kt
@@ -2,12 +2,88 @@ package com.example.cashmanager.ui.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.cashmanager.R
+import com.example.cashmanager.data.dto.UserDTO
+import com.example.cashmanager.service.LoginService
+import com.example.cashmanager.service.ServiceBuilder
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var usernameEditText : EditText
+    private lateinit var firstNameEditText : EditText
+    private lateinit var lastNameEditText : EditText
+    private lateinit var emailEditText : EditText
+    private lateinit var passwordEditText : EditText
+    private lateinit var repeatPaswordEditText : EditText
+    private lateinit var errorTextView : TextView
+    private lateinit var registerBtn : Button
+
+    private var user : UserDTO = UserDTO()
+
+    private lateinit var loginService: LoginService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        usernameEditText = findViewById(R.id.username_textview)
+        firstNameEditText = findViewById(R.id.firstname_textview)
+        lastNameEditText = findViewById(R.id.lastname_textview)
+        emailEditText = findViewById(R.id.email_editText)
+        passwordEditText = findViewById(R.id.email_editText)
+        repeatPaswordEditText = findViewById(R.id.password_repeat_editText)
+        errorTextView = findViewById(R.id.error_textView)
+        registerBtn = findViewById(R.id.register_btn)
+
+        loginService = ServiceBuilder.createService(LoginService::class.java)
+    }
+
+    /***
+     * Check if user params are valids
+     * @return if the user param are ok
+     */
+    private fun checkRegister(): Boolean {
+        if (usernameEditText.text.isNullOrBlank()) {
+            errorTextView.text = resources.getText(R.string.error_no_username)
+            return false
+        }
+        else if (passwordEditText.text.isNullOrBlank() || repeatPaswordEditText.text.isNullOrBlank()) {
+            errorTextView.text = resources.getText(R.string.error_no_password)
+            return false
+        }
+        else if (passwordEditText.text != repeatPaswordEditText.text) {
+            errorTextView.text = resources.getText(R.string.error_repeat_password)
+        }
+        errorTextView.text = ""
+        return true
+    }
+
+    /***
+     * Request the API for the creation of a user
+     */
+    fun register(v: View) {
+        if (!checkRegister())
+            return
+
+
+        val call = loginService.register(user)
+
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, respose: Response<ResponseBody>) {
+                println("test")
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                println("test")
+            }
+        })
     }
 }
